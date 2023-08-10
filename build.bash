89236efe64f23e25d10ae632479c1670d587f887
Wankupi <2893353848@qq.com>
2023-08-10 23:08:15 +0800

    build(oj-test)

--- build.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")" || exit
-root=$(pwd)
-cd resources/antlr4 || exit
-antlr4 -no-listener -visitor -o "${root}/MxAntlr" MxLexer.g4 MxParser.g4 -Dlanguage=Cpp
-cd ${root}
