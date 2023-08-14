190709f2d2687a615dcd9c0c5bb615c3a4950571
Wankupi <2893353848@qq.com>
2023-08-14 18:34:17 +0800

    test(asm): add ctest for codegen-ASM

--- run-riscv32m.bash
+++ run-riscv32m.bash
@@ -6,18 +6,19 @@ sed -n '/=== input ===/,/=== end ===/{//!p}' "$2" >test.in
 sed -n '/=== output ===/,/=== end ===/{//!p}' "$2" >test.ans
 exitCode=$(grep 'ExitCode:' "$2" | sed 's/ExitCode: //g')
 
-echo 'compile to llvm IR'
-$1 -emit-llvm $2
-echo 'compile to binary'
-clang -m32 test.ll builtin.ll -o binary 1>/dev/null 2>/dev/null
-echo 'run binary'
+echo 'compile to riscv32m'
+$1 -S $2 >test.s
 
-set +e
-./binary <test.in >test.out
-if [[ $? -ne $exitCode ]]; then
+echo 'run ravel'
+
+ravelOut=$(./ravel --oj-mode 2>&1)
+
+ec=$(grep 'exit code:' <<< "$ravelOut" | sed 's/exit code: //g')
+
+if [[ "$ec" -ne "$exitCode" ]]; then
 	echo 'exit code not match, should be ' "$exitCode" ' but got ' $?
 	exit 1
 fi
-set -e
+
 echo 'compare output'
-diff -qZB test.out test.ans
+diff -ZB test.out test.ans
